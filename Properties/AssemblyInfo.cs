80fb77b72f46f8d9323ad5a5dfd1fb262b81f02f
Serge Semenov <sergii.semenov@skyKick.com>
2017-12-09 09:40:04 -0800

    Bump version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -15,16 +15,16 @@ using System.Runtime.InteropServices;
 [assembly: Guid("2ad885d6-2eb9-483e-bf6e-e2f860964a88")]
 
 #if SDK6
-[assembly: AssemblyVersion("6.0.3.0")]
-[assembly: AssemblyFileVersion("6.0.3.0")]
+[assembly: AssemblyVersion("6.0.4.0")]
+[assembly: AssemblyFileVersion("6.0.4.0")]
 #endif
 
 #if SDK7
-[assembly: AssemblyVersion("7.0.3.0")]
-[assembly: AssemblyFileVersion("7.0.3.0")]
+[assembly: AssemblyVersion("7.0.4.0")]
+[assembly: AssemblyFileVersion("7.0.4.0")]
 #endif
 
 #if SDK8
-[assembly: AssemblyVersion("8.0.3.0")]
-[assembly: AssemblyFileVersion("8.0.3.0")]
+[assembly: AssemblyVersion("8.0.4.0")]
+[assembly: AssemblyFileVersion("8.0.4.0")]
 #endif
